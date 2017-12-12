d8723b141fa362e53fa66ae2034fa88c0d3e7f62
jbourne <jre.eros@gmail.com>
2017-12-12 19:25:34 +0100

    Add model layer

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,17 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+}
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class CrimeActivity extends AppCompatActivity {
 
