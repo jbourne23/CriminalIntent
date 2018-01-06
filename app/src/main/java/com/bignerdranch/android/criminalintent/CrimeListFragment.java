53f6ce1be1b2d2ff5905d489aee76ca72c0dd302
jbourne <jre.eros@gmail.com>
2018-01-06 19:14:53 +0100

    Click on an list's item shows a toast

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -37,16 +38,21 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(adapter);
     }
 
+    private void anim() {
+        mCrimeRecyclerView.getAdapter().notifyItemMoved(0, 5);
+    }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         private final TextView mTitleTextView;
         private final TextView mDateTextView;
 
         private Crime mCrime;
 
-        CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+            itemView.setOnClickListener(this);
 
             mTitleTextView = itemView.findViewById(R.id.crime_title);
             mDateTextView = itemView.findViewById(R.id.crime_date);
@@ -56,6 +62,12 @@ public class CrimeListFragment extends Fragment {
             mCrime = crime;
             mTitleTextView.setText(mCrime.getTitle());
             mDateTextView.setText(mCrime.getDate().toString());
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+            anim();
         }
     }
 
